0a4cf31b624f18d8560af618373147fca085936d
Fernando Ayats <ayatsfer@gmail.com>
2022-01-04 18:52:04 +0100

    First commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "My flake";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils-plus.url = github:gytis-ivaskevicius/flake-utils-plus;
+  };
+
+  outputs = inputs @ { self, nixpkgs, flake-utils-plus, ... }:
+    flake-utils-plus.lib.mkFlake {
+      inherit self inputs;
+
+
+      outputsBuilder = (channels: {
+        devShell = channels.nixpkgs.mkShell {
+          name = "my-shell";
+
+          buildInputs = with channels.nixpkgs; [
+            hugo
+          ];
+        };
+      });
+
+    };
+}
