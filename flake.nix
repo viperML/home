b765fc02d42ce5341fb6e978a21c06bb9fc3688b
Fernando Ayats <ayatsfer@gmail.com>
2022-01-04 19:23:22 +0100

    Added theme

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     flake-utils-plus.url = github:gytis-ivaskevicius/flake-utils-plus;
+
+    bookworm = {
+      url = github:gethugothemes/bookworm-light;
+      flake = false;
+    };
   };
 
   outputs = inputs @ { self, nixpkgs, flake-utils-plus, ... }:
@@ -14,6 +19,12 @@
       outputsBuilder = (channels: {
         devShell = channels.nixpkgs.mkShell {
           name = "my-shell";
+
+          shellHook = ''
+            mkdir -p themes
+            ln -s ${inputs.bookworm} themes/bookworm
+          '';
+
 
           buildInputs = with channels.nixpkgs; [
             hugo
