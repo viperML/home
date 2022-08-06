010966302126eac498234e5a6aca27c003cf7227
Fernando Ayats <ayatsfer@gmail.com>
2022-08-06 13:44:20 +0200

    move to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,59 +1,63 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
-    genSystems = nixpkgs.lib.genAttrs [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    pkgsFor = nixpkgs.legacyPackages;
-  in {
-    packages = genSystems (system: rec {
-      bookworm-light = pkgsFor.${system}.fetchFromGitHub {
-        owner = "gethugothemes";
-        repo = "bookworm-light";
-        rev = "47981c600c2c6adde3af0742c2ab352d1464f46b";
-        sha256 = "sha256-p4G8vKY/wnWpSJV0JK89R8wyZn/C6ecyrJZGkDkiDX0=";
-      };
-      themes = pkgsFor.${system}.linkFarm "themes" [
-        {
-          name = "bookworm-light";
-          path = bookworm-light;
-        }
+    flake-parts,
+  }:
+    flake-parts.lib.mkFlake {inherit self;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
       ];
-      default = with pkgsFor.${system};
-        stdenv.mkDerivation {
-          pname = "home";
-          version = self.lastModifiedDate;
-          src = self;
-          nativeBuildInputs = [
-            hugo
-            asciidoctor
-          ];
-          HUGO_THEMESDIR = themes;
-          buildPhase = ''
-            mkdir -p $out
-            hugo --minify --destination $out
-          '';
-          dontInstall = true;
+      perSystem = {
+        pkgs,
+        self',
+        ...
+      }: {
+        packages = {
+          bookworm-light = pkgs.fetchFromGitHub rec {
+            repo = "bookworm-light";
+            name = repo;
+            owner = "gethugothemes";
+            rev = "47981c600c2c6adde3af0742c2ab352d1464f46b";
+            sha256 = "sha256-p4G8vKY/wnWpSJV0JK89R8wyZn/C6ecyrJZGkDkiDX0=";
+          };
+          themes = pkgs.linkFarmFromDrvs "themes" [self'.packages.bookworm-light];
+          default = pkgs.stdenvNoCC.mkDerivation {
+            pname = "home";
+            version = self.lastModifiedDate;
+            src = self;
+            nativeBuildInputs = [
+              pkgs.hugo
+              pkgs.asciidoctor
+            ];
+            HUGO_THEMESDIR = self'.packages.themes;
+            buildPhase = ''
+              mkdir -p $out
+              hugo --minify --destination $out
+            '';
+            dontInstall = true;
+          };
         };
-      serve = with pkgsFor.${system};
-        writeShellScriptBin "serve" ''
-          ${ran}/bin/ran -r ${default}
-        '';
-    });
-    devShells = genSystems (system: {
-      default = pkgsFor.${system}.mkShellNoCC {
-        name = "hugo-devshell";
-        inputsFrom = [self.packages.${system}.default];
-        packages = [
-          pkgsFor.${system}.nomad
-        ];
-        HUGO_THEMESDIR = self.packages.${system}.themes;
+        devShells.default = pkgs.mkShellNoCC {
+          name = "home";
+          inputsFrom = [
+            self'.packages.default
+          ];
+          packages = [
+            pkgs.nomad
+          ];
+          HUGO_THEMESDIR = self'.packages.themes;
+        };
       };
-    });
-  };
+    };
 }
